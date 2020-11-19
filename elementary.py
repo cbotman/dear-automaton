c064531ada9d3b592b07fe33b2b8ed0337efb567
Chris Botman <chris@humaan.com.au>
2020-11-19 10:25:00 +0800

    Update: Can now set character used to render on and off cells.

--- elementary.py
+++ elementary.py
@@ -35,35 +35,43 @@ def update_state(state):
     return value
 
 
-def render(value):
-    print(value.replace('0', ' ').replace('1', 'X'))
+def render(value, off_char, on_char):
+    print(value.replace('0', off_char).replace('1', on_char))
 
 
 def main(args):
     # initial state
     state = args.state
     while not state or not is_binary_string(state):
-        state = input("🤖 Enter an initial string of ones and zeroes: ")
+        state = input('🤖 Enter an initial string of ones and zeroes: ')
 
     # left padding initial state
     required_padding = max(args.pad_left - len(state), 0)
     for i in range(0, required_padding):
         state = '0' + state
 
+    # characters for console rendering
+    off_char = args.off[0]
+    on_char = args.on[0]
+
     # loop
-    render(state)
+    render(state, off_char, on_char)
     while True:
         state = update_state(state)
-        render(state)
+        render(state, off_char, on_char)
         sleep(0.25)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--state", help="set the initial state (e.g. --state 01101110)", type=str)
-parser.add_argument("-p", "--pad-left", default=0, help="pads the initial state (wtih zeroes) to this length "
-                                                        "(e.g. --pad-left=20)", type=int)
+parser.add_argument('-s', '--state', help='set the initial state (e.g. --state 01101110)', type=str)
+parser.add_argument('-p', '--pad-left', default=0, help='pads the initial state (wtih zeroes) to this length '
+                                                        '(e.g. --pad-left=20)', type=int)
+parser.add_argument('--off', help='character to show when a cell is off (e.g. default is a blank space)', default=' ',
+                    type=str)
+parser.add_argument('--on', help='character to show when a cell is on (e.g. default is X)', default='X',
+                    type=str)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parser.parse_args()
     try:
         main(args)
